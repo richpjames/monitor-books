3578377c9130e26cb7f2e910d79bd38d8e0194cb
ricojamjar <rich@genericgreeting.co.uk>
2019-08-24 13:53:03 +0100

    styling navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,20 @@ import styled from "styled-components";
 
 const Nav = styled.nav`
   display: flex;
+  width: 70%;
+  justify-content: space-between;
+  border-bottom: 1px solid black;
+  margin-left: auto;
+  margin-right:auto;
+  margin-top: 10vh;
 `;
-const Logo = styled.img``;
+const Logo = styled.img`
+  height: 100px;
+`;
+const NavLinks = styled.ul`
+  list-style: none;
+`;
+const NavItem = styled.li``;
 
 const NavBar = () => {
   return (
@@ -13,6 +25,10 @@ const NavBar = () => {
         className="Logo"
         src="https://www.monitorbooks.co.uk/img/monitor-bw-resized.jpg"
       />
+      <NavLinks>
+        <NavItem>Contact</NavItem>
+        <NavItem>About</NavItem>
+      </NavLinks>
     </Nav>
   );
 };
