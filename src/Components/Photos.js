bf8a36d974c553cfd4545aa2152493b34fc9d738
ricojamjar <rich@genericgreeting.co.uk>
2019-08-24 13:53:17 +0100

    creating photo component and adding min-height

---
+++ src/Components/Photos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "styled-components"
+
+const PhotoWrap = styled.section`min-height: 80vh;`
+
+
+export default function Photos() {
+  return (
+    <PhotoWrap/>
+  )
+}
