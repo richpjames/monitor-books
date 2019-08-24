9c140b81cb02a73626d738c71b904941eefcc039
ricojamjar <rich@genericgreeting.co.uk>
2019-08-24 13:52:34 +0100

    displaying intro slide for development

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react'
-import Slideshow from "./Components/Slideshow"
-import MurmurBook from "./Components/MurmurBook"
+import React, { Component } from "react";
+import Slideshow from "./Components/Slideshow";
+import MurmurBook from "./Components/MurmurBook";
 
 export default class App extends Component {
   state = {
     showImage: true
-  }
+  };
   render() {
-    const {showImage} = this.state;
-    return (<>{showImage ? <Slideshow/> : <MurmurBook/>}</>)
+    const { showImage } = this.state;
+    return (
+      <>
+        {
+          /* <>{showImage ? <Slideshow/> :  */
+        }
+        <MurmurBook />
+      </>
+    );
   }
   componentDidMount = () => {
     setTimeout(
-      function(){
-      this.setState({showImage: false});
-    }
-    .bind(this),
-    3000
-    )
-  }
+      function() {
+        this.setState({ showImage: false });
+      }.bind(this),
+      3000
+    );
+  };
 }
