f05cde6cae85a92affdcb10e63775deae25e21f3
ricojamjar <rich@genericgreeting.co.uk>
2019-08-24 13:52:52 +0100

    adding Text and Photo component to Bookpage

--- src/Components/MurmurBook.js
+++ src/Components/MurmurBook.js
@@ -1,8 +1,16 @@
 import React from "react";
+import styled from "styled-components"
 import NavBar from "./NavBar";
+import Photos from "./Photos"
+import Text from "./Text"
+
+const BookWrap = styled.div`
+width: 70vw;
+margin: 0 auto;`
 
 const MurmurBook = () => {
-  return <NavBar />;
+  return (<><NavBar /><BookWrap><Photos/>
+    <Text/></BookWrap></>);
 };
 
 export default MurmurBook;
