a24dffff6a35d151187f6cc0ebdc38c6b340bbb2
ricojamjar <rich@genericgreeting.co.uk>
2019-08-24 13:53:28 +0100

    creating text component and adding text

---
+++ src/Components/Text.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from "styled-components";
+
+const LeftSection = styled.div``
+const RightSection = styled.div``
+const TextWrapper = styled.section``
+
+const Text = () => {
+  return (
+    <p>
+      
+
+Murmur Anthology #1 presents new work from contributors to the Murmur Reading Series in Manchester. Featuring poetry, prose, and drama from seventeen international authors, this is a varied and exciting collection of innovative contemporary writing.
+
+Contributors: Rachael Allen, Bryony Bates, Jen Calleja, Alan Fielden, Joey Frances, Aurelia Guo, Tessa Harris, Tom Jenks, Barbara Juch, Sophie Jung, Shiv Kotecha, Jazmine Linklater, Amy McCauley, Lila Matsumoto, Sam Riviere, Michelle Steinbeck, Mónica de la Torre.
+
+Edited by Rory Cook, with Harriet Hill-Payne, Lucy Burns & RL Perry.
+Designed by Joe Haigh at Chaosmos Studios.
+Artwork by Joe Haigh & Michael Holland.
+122 pages.
+Perfect bound with tracing paper wrap.
+Edition of 300.
+
+
+Published June 2019.
+£12 
+    </p>
+  );
+}
+
+export default Text;
