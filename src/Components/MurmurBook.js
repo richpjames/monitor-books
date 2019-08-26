86af451ea347905d55ae19e6db8f1799cb8304a2
ricojamjar <rich@genericgreeting.co.uk>
2019-08-26 21:01:24 +0100

    adding footer and putting navbar in wrap

--- src/Components/MurmurBook.js
+++ src/Components/MurmurBook.js
@@ -1,16 +1,24 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import NavBar from "./NavBar";
-import Photos from "./Photos"
-import Text from "./Text"
+import Photos from "./Photos";
+import Text from "./Text";
+import Footer from "./Footer";
 
 const BookWrap = styled.div`
-width: 70vw;
-margin: 0 auto;`
+  width: 70vw;
+  margin: 0 auto;
+`;
 
 const MurmurBook = () => {
-  return (<><NavBar /><BookWrap><Photos/>
-    <Text/></BookWrap></>);
+  return (
+    <BookWrap>
+      <NavBar />
+      <Photos />
+      <Text />
+      <Footer />
+    </BookWrap>
+  );
 };
 
 export default MurmurBook;
