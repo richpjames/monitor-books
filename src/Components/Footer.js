e078e20f6f57dff93d2dec8ddc0faafc5e8f923b
ricojamjar <rich@genericgreeting.co.uk>
2019-08-26 21:01:04 +0100

    adding footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterWrap = styled.section`
+  border-top: 1px black solid;
+  margin-bottom: 5%;
+  margin-top: 5%;
+`;
+
+const Footer = () => {
+  return <FooterWrap></FooterWrap>;
+};
+
+export default Footer;
