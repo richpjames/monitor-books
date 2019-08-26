07e7a86c6d1cd2550209fcb3985aac0dba637aad
ricojamjar <rich@genericgreeting.co.uk>
2019-08-26 21:03:00 +0100

    styling navbar and adding svg logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,19 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "./Logo";
 
 const Nav = styled.nav`
   display: flex;
-  width: 70%;
+  width: 100%;
   justify-content: space-between;
   border-bottom: 1px solid black;
   margin-left: auto;
-  margin-right:auto;
+  margin-right: auto;
   margin-top: 10vh;
-  font-family: "GT America", sans-serif;
 `;
-const Logo = styled.img`
-  height: 100px;
-`;
+
 const NavLinks = styled.ul`
   list-style: none;
 `;
@@ -22,12 +20,11 @@ const NavItem = styled.li``;
 const NavBar = () => {
   return (
     <Nav className="Nav">
-      <Logo
-        className="Logo"
-        src="https://www.monitorbooks.co.uk/img/monitor-bw-resized.jpg"
-      />
+      <Logo className="Logo" />
       <NavLinks>
-        <NavItem> <a href="mailto:editor@monitorbooks.co.uk">Contact</a></NavItem>
+        <NavItem>
+          <a href="mailto:editor@monitorbooks.co.uk">Contact</a>
+        </NavItem>
         <NavItem>About</NavItem>
       </NavLinks>
     </Nav>
