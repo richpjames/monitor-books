d991b9cb4365145df596f410a8a1ef4438d94f41
ricojamjar <rich@genericgreeting.co.uk>
2019-09-15 17:05:17 +0100

    adding styling

--- src/Components/IndividualPhoto.js
+++ src/Components/IndividualPhoto.js
@@ -2,16 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import VisibilitySensor from "react-visibility-sensor";
 
-const Image = styled.img`
-  padding: 10px;
+const Photo = styled.img`
+  min-width: 45%;
+  display: flex;
+  margin: 2%;
+  max-height: 20vw;
+  @media screen and (max-width: 500px) {
+    margin: 0.5%;
+    width: 100%;
+  }
 `;
-
 export default function IndividualPhoto(props) {
   const { openLightbox, photo, index } = props;
 
   return (
     <VisibilitySensor>
-      <Image
+      <Photo
         onClick={() => openLightbox(index)}
         src={photo}
         alt="photo of tape"
