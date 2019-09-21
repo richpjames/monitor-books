1fc8ccf76c2079aac66af9987669c539874d9d53
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2019-09-21 11:02:48 +0100

    a build

---
+++ build/precache-manifest.7da3956973c48c48ca6a519b0a540f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc9c1f90d9b7f2e85f09c2bcefa16ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfaed4ca6ed722817015",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "4bd9c47fde0b67400d15",
+    "url": "/static/css/main.d6fbda73.chunk.css"
+  },
+  {
+    "revision": "bfaed4ca6ed722817015",
+    "url": "/static/js/2.c88cc25f.chunk.js"
+  },
+  {
+    "revision": "4bd9c47fde0b67400d15",
+    "url": "/static/js/main.f68eea7b.chunk.js"
+  },
+  {
+    "revision": "0dab27b8fc842a5c3849",
+    "url": "/static/js/runtime~main.d0bba706.js"
+  }
+]);
