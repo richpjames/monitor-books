a66f30089410a3c4b510c4d6457a0cd37a4520cc
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2019-09-21 10:59:48 +0100

    Adding About text

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styled from 'styled-components'
+
+const TextWrap = styled.p`
+margin-left: auto;
+margin-right: auto;
+padding-top: 5vh;
+width: 50%;`
 
 function About() {
-    return (
-        <p>word</p>
-    )
+  return (
+    <TextWrap>
+      Monitor is a publication platform for poetry and innovative writing based
+      in Manchester, UK. Its first publication, Murmur Anthology #1, is
+      published September 2019. Single-author releases are forthcoming from the
+      autumn
+    </TextWrap>
+  );
 }
 
-export default About
+export default About;
