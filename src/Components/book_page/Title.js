8b883e004d0de9e1732a0642cfc6e1518f634d09
Richard James <richardjames@Richards-MacBook-Pro-2.local>
2019-09-22 12:14:57 +0100

    adding custom font

--- src/Components/book_page/Title.js
+++ src/Components/book_page/Title.js
@@ -2,9 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const TitleText = styled.h1`
+@font-face {
+    font-family: "Caslon";
+    src: url(https://www.richjames.co.uk/fonts/ACaslonPro-Italic.otf) format("truetype");
+}
+
 padding-top: 5vh;
 padding-bottom: 3vh;
-font-size: 1.5em;
+font-size: 2em;
 width: 100%;
 margin-top: 0;
 margin-right: 0;
@@ -12,13 +17,7 @@ margin-bottom: 0;
 margin-left: 2.54%;
 font-family: "Caslon", sans-serif;
 text-align: left;
-@font-face {
-    font-family: "Caslon";
-    src: url(ACaslonPro-Italic.otf) format("truetype");
-}
-p.customfont { 
-    font-family: "My Custom Font", Verdana, Tahoma;
-}
+
 `;
 
 const TitleWrap = styled.section`
