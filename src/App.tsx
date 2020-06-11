d259f2d9e9a1c55daa4b23c4919ad23cd76311a1
Richard James <rich@Richards-MacBook-Pro.local>
2020-06-11 20:18:30 +0100

    convert to TS and add hooks

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Slideshow from "./Components/Slideshow";
+import MainPage from "./Components/MainPage";
+
+const App = () => {
+  const [showImage, setShowImage] = useState(true);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setShowImage(false);
+    }, 3000);
+  }, []);
+
+  return <> {showImage ? <Slideshow /> : <MainPage />} </>;
+};
+
+export default App;
