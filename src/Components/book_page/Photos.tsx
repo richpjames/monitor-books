12363dd9c9dcf883fa448aea4bc6eb4523819652
Richard James <rich@Richards-MacBook-Pro.local>
2020-06-12 13:57:07 +0100

    convert to typescript

---
+++ src/Components/book_page/Photos.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+import IndividualPhoto from "./IndividualPhoto";
+
+const PhotoWrap = styled.section`
+  min-height: 50vh;
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 100%;
+  margin-top: 3vh;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const photoNumbers = [2, 3, 5, 6];
+const mainURL = "https://www.monitorbooks.co.uk/img/ant_img/";
+const thumbURL = `https://www.monitorbooks.co.uk/img/ant_img/thumbnails/`;
+const imageUrls = photoNumbers.map((photo) => `${mainURL}${photo}.jpg`);
+const thumbUrls = photoNumbers.map((photo) => `${thumbURL}${photo}.jpg`);
+
+interface IProps {
+  photos: [number, number, number, number];
+}
+
+const Photos = (props: IProps) => {
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [galleryOpen, setGalleyOpen] = useState(false);
+
+  const openLightbox = (i: number) => {
+    setPhotoIndex(i);
+    setGalleyOpen(true);
+  };
+
+  const thumbReel = thumbUrls.map((url, i) => {
+    return (
+      <IndividualPhoto
+        openLightbox={openLightbox}
+        index={i}
+        photo={url}
+        key={url}
+      />
+    );
+  });
+
+  return (
+    <>
+      <PhotoWrap className="ImagesWrapper">{thumbReel}</PhotoWrap>
+      {galleryOpen && (
+        <Lightbox
+          mainSrc={imageUrls[photoIndex]}
+          nextSrc={imageUrls[(photoIndex + 1) % imageUrls.length]}
+          prevSrc={
+            imageUrls[(photoIndex + imageUrls.length - 1) % imageUrls.length]
+          }
+          onCloseRequest={() => setGalleyOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex(
+              (photoIndex + imageUrls.length - 1) % imageUrls.length
+            )
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % imageUrls.length)
+          }
+        />
+      )}
+    </>
+  );
+};
+
+export default Photos;
