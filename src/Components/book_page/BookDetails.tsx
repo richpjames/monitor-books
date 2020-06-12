263eaa63c824a6763ab8b66c8cd572f20ee29a08
Richard James <rich@Richards-MacBook-Pro.local>
2020-06-12 13:56:55 +0100

    add types and plumb props through

--- src/Components/book_page/BookDetails.tsx
+++ src/Components/book_page/BookDetails.tsx
@@ -7,15 +7,24 @@ import Title from "./Title";
 import BuyButton from "./BuyButton";
 
 interface IProps extends RouteComponentProps {
-  book: any;
+  book: {
+    title: String;
+    author: String;
+    photos: Number[];
+    blurb1: String;
+    blurb2: String;
+    price: String;
+    publishDate: String;
+  };
 }
 
 const BookDetails = (props: IProps) => {
+  const { photos, title, author, blurb1, blurb2 } = props.book;
   return (
     <>
-      <Photos />
-      <Title title="" />
-      <Text />
+      <Photos photos={[1, 2, 3, 4]} />
+      <Title title={title} author={author} />
+      <Text leftText={blurb1} rightText={blurb2} />
       <BuyButton />
     </>
   );
