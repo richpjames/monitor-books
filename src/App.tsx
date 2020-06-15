e706c58d178bd5e19a2e52d68e1ae09872a0f085
Richard James <rich@Richards-MacBook-Pro.local>
2020-06-15 21:21:03 +0100

    remove slide show logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import Slideshow from "./Components/Slideshow";
 import MainPage from "./Components/MainPage";
 
 const App = () => {
-  const [showImage, setShowImage] = useState(false);
-
-  useEffect(() => {
-    setTimeout(function () {
-      setShowImage(false);
-    }, 3000);
-  }, []);
-
   return <MainPage />;
 };
 
