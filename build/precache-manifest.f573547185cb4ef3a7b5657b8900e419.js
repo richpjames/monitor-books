e4577bcbf1c771c794e5beed0f87cdc78470329c
Richard James <rich@Richards-MacBook-Pro.local>
2020-06-28 21:51:51 +0100

    add

---
+++ build/precache-manifest.f573547185cb4ef3a7b5657b8900e419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7938cd8a1f91ba3b9b3c4f9f7ed2d7e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "649629ac069e1ab043d6",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "07c309e0a85aabb5f5db",
+    "url": "/static/css/main.0861851d.chunk.css"
+  },
+  {
+    "revision": "649629ac069e1ab043d6",
+    "url": "/static/js/2.b8b6a19d.chunk.js"
+  },
+  {
+    "revision": "9690d358bfa0b906346a6a94f4900583",
+    "url": "/static/js/2.b8b6a19d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c309e0a85aabb5f5db",
+    "url": "/static/js/main.aaca403b.chunk.js"
+  },
+  {
+    "revision": "55953f25030c423795c9",
+    "url": "/static/js/runtime-main.0a7277cc.js"
+  }
+]);
