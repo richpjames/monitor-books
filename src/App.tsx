03a0dbb1caeb5e4019ff8644ef87c42af1d48190
Richard James <rich@Richards-MacBook-Pro.local>
2020-06-28 20:39:52 +0100

    reinstate intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import MainPage from "./Components/MainPage";
+import Slideshow from "./Components/Slideshow";
 
-const App = () => <MainPage />;
+const App = () => {
+  const [showImage, setShowImage] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowImage(false);
+    }, 3000);
+  }, []);
+
+  return <>{showImage ? <Slideshow /> : <MainPage />}</>;
+};
 
 export default App;
