5c12964c0381cc76543acf08c4730f77fbc4ce45
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-01 21:37:24 +0100

    turn off slideshow for edits

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MainPage from "./Components/MainPage";
 import Slideshow from "./Components/Slideshow";
 
 const App = () => {
-  const [showImage, setShowImage] = useState(true);
+  const [showImage, setShowImage] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
