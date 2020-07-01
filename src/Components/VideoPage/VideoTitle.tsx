d74e4c2b238f0baee729ad94e83f7269779202d8
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-01 21:53:50 +0100

    make decent

--- src/Components/VideoPage/VideoTitle.tsx
+++ src/Components/VideoPage/VideoTitle.tsx
@@ -1,34 +1,19 @@
 import React from "react";
-import styled from "styled-components/macro";
 
-import { AmericaTitle } from "../Common/Common";
-
-const Container = styled.div`
-  border-bottom: 1px solid black;
-  margin-bottom: 20px;
-`;
-
-const TitleCopy = styled.h1`
-  display: inline;
-  font-size: 1em;
-  font-weight: 300;
-`;
+import { AmericaTitleBold, AmericaTitle, TitleWrapper } from "../Common/Common";
 
 interface IProps {
   title: string;
   subtitle: string;
-  split?: boolean;
 }
 
 function VideoTitle(props: IProps) {
   const { title, subtitle } = props;
   return (
-    <Container>
-      <AmericaTitle>
-        <TitleCopy>{subtitle}: </TitleCopy>
-        {title}
-      </AmericaTitle>
-    </Container>
+    <TitleWrapper>
+      <AmericaTitle>{title}: </AmericaTitle>
+      <AmericaTitleBold>{subtitle}</AmericaTitleBold>
+    </TitleWrapper>
   );
 }
 
