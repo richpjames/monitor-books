0201202990eb0b6cd309f0fc99bc63c7e358407e
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-01 21:37:46 +0100

    add styles to common

--- src/Components/Common/Common.tsx
+++ src/Components/Common/Common.tsx
@@ -1,18 +1,25 @@
 import styled from "styled-components";
 
+const fontSize = 3;
+
 export const PageContainer = styled.div`
   max-width: 100%;
   margin-top: 3vh;
 `;
 
 export const CalsonTitle = styled.h1`
-  margin-left: 2%;
-  margin-right: 2%;
+  font-size: ${fontSize}em;
+  display: inline;
+  font-family: "Caslon", sans-serif;
+  font-weight: 300;
 `;
 
 export const AmericaTitle = styled(CalsonTitle)`
+  font-weight: 200;
+  font-family: "GT America", sans-serif;
+  font-size: ${fontSize}em;
   margin-left: 2.5%;
-  font-size: 2em;
+  display: inline;
   @media only screen and (max-width: 700px) {
     font-size: 1.1em;
   }
