082013f25bccd93a10e9f58d6ccbee460c2cf235
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-01 21:38:08 +0100

    remove nested h1s

--- src/Components/Common/Title.tsx
+++ src/Components/Common/Title.tsx
@@ -1,35 +1,10 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-const fontSize = 0.9;
+import { CalsonTitle, AmericaTitle } from "./Common";
 
-const TitleText = styled.h1`
-  font-weight: 200;
-  padding-top: 5vh;
-  padding-bottom: 3vh;
-  font-size: ${fontSize * 3}em;
-  width: 95%;
-  margin-top: 0;
-  margin-right: 0;
-  margin-bottom: 0;
-  margin-left: 2.54%;
-  font-family: "Caslon", sans-serif;
-  text-align: left;
+const TitleWrapper = styled.div`
   border-bottom: 1px solid black;
-  @media only screen and (max-width: 700px) {
-    font-size: ${fontSize * 1.6}em;
-  }
-`;
-
-const TitleCopy = styled.h1`
-  display: inline;
-  font-family: "GT America", sans-serif;
-  font-size: ${fontSize * 1.15}em;
-  font-weight: 300;
-  @media only screen and (max-width: 700px) {
-    display: inline;
-    font-size: ${fontSize}em;
-  }
 `;
 
 interface IProps {
@@ -41,10 +16,10 @@ interface IProps {
 function Title(props: IProps) {
   const { title, subtitle } = props;
   return (
-    <TitleText>
-      <TitleCopy>{subtitle} </TitleCopy>
-      {title}
-    </TitleText>
+    <TitleWrapper>
+      <AmericaTitle>{title} </AmericaTitle>
+      <CalsonTitle>{subtitle}</CalsonTitle>
+    </TitleWrapper>
   );
 }
 
