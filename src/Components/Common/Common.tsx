c33fa1645c490eb12d1cefe99b03c497208c79a0
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-02 08:25:11 +0100

    split titles style from common

--- src/Components/Common/Common.tsx
+++ src/Components/Common/Common.tsx
@@ -1,33 +1,12 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
-const fontSize = 2;
-
-export const PageContainer = styled.div`
+export const PageWrapper = styled.div`
   max-width: 100%;
   margin-top: 3vh;
 `;
 
-export const CalsonTitle = styled.h1`
-  font-size: ${fontSize}em;
-  display: inline;
-  font-family: "Caslon", sans-serif;
-  font-weight: 300;
-`;
-
-export const AmericaTitle = styled(CalsonTitle)`
-  font-weight: 200;
-  font-family: "GT America", sans-serif;
-  font-size: ${fontSize}em;
-  display: inline;
-  @media only screen and (max-width: 700px) {
-    font-size: 1.1em;
-  }
-`;
-
-export const AmericaTitleBold = styled(AmericaTitle)`
-  font-weight: 400;
-`;
-
-export const TitleWrapper = styled.div`
-  border-bottom: 1px solid black;
+export const InfoSection = styled.div`
+  width: 95%;
+  margin-left: auto;
+  margin-right: auto;
 `;
---
+++ src/Components/Common/Titles.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components/macro";
+
+const fontSize = 2;
+
+export const CalsonTitle = styled.h1`
+  font-size: ${fontSize}em;
+  display: inline;
+  font-family: "Caslon", sans-serif;
+  font-weight: 300;
+`;
+
+export const AmericaTitle = styled(CalsonTitle)`
+  font-weight: 200;
+  font-family: "GT America", sans-serif;
+  font-size: ${fontSize}em;
+  display: inline;
+  @media only screen and (max-width: 700px) {
+    font-size: 1.1em;
+  }
+`;
+
+export const AmericaTitleBold = styled(AmericaTitle)`
+  font-weight: 400;
+`;
+
+export const TitleWrapper = styled.div`
+  padding-bottom: 2.5%;
+  padding-top: 2.5%;
+  border-bottom: 1px solid black;
+`;
