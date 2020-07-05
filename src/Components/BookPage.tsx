bdd5b4eef755b416ac42d78b3cd76081fbb5d79e
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-05 16:47:09 +0100

    change folders

--- src/Components/BookPage.tsx
+++ src/Components/BookPage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
 
-import Photos from "../Common/Photos";
-import Title from "../Common/Title";
-import Text from "../Common/Text";
-import { PageWrapper, InfoSection } from "../Common/Common";
-import BuyButton from "../Common/BuyButton";
+import Photos from "./Common/Photos";
+import Title from "./Common/Title";
+import Text from "./Common/Text";
+import { PageWrapper, InfoSection } from "./Common/Common";
+import BuyButton from "./Common/BuyButton";
 
 interface IProps extends RouteComponentProps {
   book: Book;
