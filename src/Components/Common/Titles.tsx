3fb95f9035a93991c1fa69ee0304038b57f894e6
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-14 17:53:15 +0100

    move styles to css file

--- src/Components/Common/Titles.tsx
+++ src/Components/Common/Titles.tsx
@@ -1,25 +1,13 @@
 import styled from "styled-components/macro";
 
-const fontSize = 2;
-
 export const CalsonTitle = styled.h1`
-  font-size: ${fontSize}em;
-  display: inline;
   font-family: "Caslon", sans-serif;
   font-weight: 300;
-  @media only screen and (max-width: 700px) {
-    font-size: ${fontSize - 0.5}em;
-  }
 `;
 
 export const AmericaTitle = styled(CalsonTitle)`
   font-weight: 200;
   font-family: "GT America", sans-serif;
-  font-size: ${fontSize}em;
-  display: inline;
-  @media only screen and (max-width: 700px) {
-    font-size: ${fontSize - 0.5}em;
-  }
 `;
 
 export const AmericaTitleBold = styled(AmericaTitle)`
