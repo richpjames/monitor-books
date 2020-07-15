55bdd5894218eb93f28a8d02293b5da3c3d7d72e
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-15 20:53:34 +0100

    remove icons from gitignore

---
+++ build/precache-manifest.9f84a5ad60d4699983372bcb4fbec684.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be117227182424a47677b6f8f142ef1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4636e89d7313ac1147fa",
+    "url": "./static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "115ea91f4aaa6ba24e75",
+    "url": "./static/css/main.be541a35.chunk.css"
+  },
+  {
+    "revision": "4636e89d7313ac1147fa",
+    "url": "./static/js/2.ca9d73a0.chunk.js"
+  },
+  {
+    "revision": "9690d358bfa0b906346a6a94f4900583",
+    "url": "./static/js/2.ca9d73a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115ea91f4aaa6ba24e75",
+    "url": "./static/js/main.5ffbe088.chunk.js"
+  },
+  {
+    "revision": "42e5fa5d2f723d11490e",
+    "url": "./static/js/runtime-main.4be6964a.js"
+  },
+  {
+    "revision": "8dfe9005e8225c8227d55cfbf695729b",
+    "url": "./static/media/logo.8dfe9005.png"
+  }
+]);
