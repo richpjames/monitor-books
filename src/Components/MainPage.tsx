1131ff9c00e7781b49719743ee5fabfc9a8cf19b
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-16 08:50:17 +0100

    create basket page and add routing

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -3,17 +3,19 @@ import styled from "styled-components/macro";
 import { Router } from "@reach/router";
 
 import { getBooks, getVideos } from "../Hooks";
-import VideoPage from "./VideoPage";
-import NavBar from "./NavBar";
-import BookDetails from "./BookPage";
+import About from "./Pages/About";
+import Basket from "./Pages/Basket";
+import BookDetails from "./Pages/BookPage";
 import Footer from "./Footer";
-import About from "./About";
+import NavBar from "./NavBar";
+import VideoPage from "./Pages/VideoPage";
 
 const PageWrap = styled.div`
-  width: 95%;
   margin-left: auto;
   margin-right: auto;
   max-width: 100vw;
+  width: 95%;
+
   @media only screen and (min-width: 600px) {
     width: 60%;
   }
@@ -39,6 +41,7 @@ const MainPage = () => {
           {videos?.map((video) => (
             <VideoPage path={video.slug} video={video} key={video.slug} />
           ))}
+          <Basket path="/basket" />
           <About path="/about" />
         </Router>
         <Footer />
---
+++ src/Components/Pages/Basket.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+interface IProps extends RouteComponentProps {}
+
+const Basket = (props: IProps) => {
+  return (
+    <div>
+      <h1>Basket</h1>
+    </div>
+  );
+};
+
+export default Basket;
