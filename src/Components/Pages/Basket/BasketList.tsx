967e8b99ee52f725284066980750b1dbc0e7ea15
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-16 21:53:50 +0100

    addd basket list

---
+++ src/Components/Pages/Basket/BasketList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import BasketListItem from "./BasketListItem";
+
+const BasketListContainer = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const BasketList = () => {
+  return (
+    <BasketListContainer>
+      <BasketListItem />
+      <p>Total: $20</p>
+    </BasketListContainer>
+  );
+};
+
+export default BasketList;
