5a77f124592766885e82ce78aaf9650910ccbd83
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-16 08:50:28 +0100

    use styled-components/macro

--- src/Components/Common/BuyButton.jsx
+++ src/Components/Common/BuyButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 //USED TO CREATE BUTTON IMG
 // const FullWidthWrap = styled.div`
 //   margin-left: 4%;
--- src/Components/Common/IndividualPhoto.tsx
+++ src/Components/Common/IndividualPhoto.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import VisibilitySensor from "react-visibility-sensor";
 
 const Photo = styled.img`
