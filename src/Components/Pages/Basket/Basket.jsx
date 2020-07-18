694e5a2d47e2ad36db7b062e980adff9006afa3d
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-18 17:36:58 +0100

    convert to js and add redux

---
+++ src/Components/Pages/Basket/Basket.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import BasketListItem from "./BasketListItem";
+
+const Basket = ({ products, total, onCheckoutClicked }) => {
+  const hasProducts = products?.length > 0;
+  const nodes = hasProducts ? (
+    products.map((product) => (
+      <BasketListItem
+        title={product.title}
+        price={product.price}
+        quantity={product.quantity}
+        key={product.id}
+      />
+    ))
+  ) : (
+    <em>Please add some products to cart.</em>
+  );
+
+  return (
+    <div>
+      <h3>Your Cart</h3>
+      <div>{nodes}</div>
+      <p>Total: &#36;{total}</p>
+      <button
+        onClick={onCheckoutClicked}
+        disabled={hasProducts ? "" : "disabled"}
+      >
+        Checkout
+      </button>
+    </div>
+  );
+};
+
+Basket.propTypes = {
+  products: PropTypes.array,
+  total: PropTypes.string,
+  onCheckoutClicked: PropTypes.func,
+};
+
+export default Basket;
