547143fdc4e78ec2810d24515c58f634aea5e7a6
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-18 17:37:35 +0100

    add redux

--- src/Components/Pages/BookPage.tsx
+++ src/Components/Pages/BookPage.tsx
@@ -1,22 +1,20 @@
 import React, { Dispatch } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { connect } from "react-redux";
 
 import Photos from "../Common/Photos";
 import Title from "../Common/Title";
 import Text from "../Common/Text";
 import { PageWrapper, InfoSection } from "../Common/Common";
 import BuyButton from "../Common/BuyButton";
-import { addToCart } from "../../redux/actions";
 
 interface IProps extends RouteComponentProps {
   book: Book;
   addBookToBasket: Dispatch<string>;
 }
 
-const BookDetails = (props: IProps) => {
-  const { addBookToBasket, book } = props;
-  const { photos, title, author, blurb1, blurb2, id } = book;
+const BookPage = (props: IProps) => {
+  const { book, addBookToBasket } = props;
+  const { photos, title, author, blurb1, blurb2 } = book;
 
   return (
     <PageWrapper>
@@ -25,13 +23,9 @@ const BookDetails = (props: IProps) => {
         <Title title={author} subtitle={title} />
         <Text leftText={blurb1} rightText={blurb2} />
       </InfoSection>
-      <BuyButton id={id} addToBasket={addBookToBasket} />
+      <BuyButton addToBasket={addBookToBasket} product={book} />
     </PageWrapper>
   );
 };
 
-const mapDispatchToProps = (dispatch: any) => ({
-  addBookToBasket: (id: string) => dispatch(addToCart(id)),
-});
-
-export default connect(null, mapDispatchToProps)(BookDetails);
+export default BookPage;
