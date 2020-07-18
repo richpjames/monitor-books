920517597543ea02eb67c051677380537c1b87e4
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-18 17:34:37 +0100

    remove getBooks hook

--- src/Hooks.ts
+++ src/Hooks.ts
@@ -1,9 +1,4 @@
-import { books } from "./Data/books.json";
 import { videos } from "./Data/videos.json";
-
-export const getBooks = (): Book[] => {
-  return books;
-};
 
 export const getVideos = (): Video[] => {
   return videos;
