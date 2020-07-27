5ecc8b6cc6b2f0de0a527a01a1aba3a7c12937a8
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-27 10:50:55 +0100

    format and add video

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cart, * as fromCart from "./cart";
 import products, * as fromProducts from "./products";
+import { videos } from "./videos";
 
 export default combineReducers({
-    cart,
-    products,
+  cart,
+  products,
+  videos,
 });
 
 const getAddedIds = (state) => fromCart.getAddedIds(state.cart);
@@ -12,16 +14,16 @@ const getQuantity = (state, id) => fromCart.getQuantity(state.cart, id);
 const getProduct = (state, id) => fromProducts.getProduct(state.products, id);
 
 export const getTotal = (state) =>
-    getAddedIds(state)
+  getAddedIds(state)
     .reduce(
-        (total, id) =>
+      (total, id) =>
         total + getProduct(state, id).price * getQuantity(state, id),
-        0
+      0
     )
     .toFixed(2);
 
 export const getCartProducts = (state) =>
-    getAddedIds(state).map((id) => ({
-        ...getProduct(state, id),
-        quantity: getQuantity(state, id),
-    }));
+  getAddedIds(state).map((id) => ({
+    ...getProduct(state, id),
+    quantity: getQuantity(state, id),
+  }));
