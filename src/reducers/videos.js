8429fa07f5b9bd1fbf2ab4c1b2802bccf798d213
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-27 21:33:49 +0100

    fix tests

--- src/reducers/videos.js
+++ src/reducers/videos.js
@@ -4,9 +4,10 @@ export const videos = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_VIDEOS:
       return action.videos;
+
     default:
       return state;
   }
 };
 
-export const getVideos = (state) => state.videos;
+export const getVideos = (state, action) => videos({ ...state }, action);
