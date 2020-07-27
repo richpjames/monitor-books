018e3cb2cd6771fba2f6de4a73b772cf990445f2
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-27 22:02:16 +0100

    add reducers

--- src/reducers/videos.js
+++ src/reducers/videos.js
@@ -1,13 +1,50 @@
 import { RECEIVE_VIDEOS } from "../constants/actionTypes";
+import { combineReducers } from "redux";
 
 export const videos = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_VIDEOS:
-      return action.videos;
-
+      return { ...state, ...action.videos };
     default:
       return state;
   }
 };
 
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_VIDEOS:
+      return {
+        ...state,
+        ...action.videos.reduce((obj, product) => {
+          obj[product.id] = product;
+          return obj;
+        }, {}),
+      };
+    default:
+      const { videoId } = action;
+      if (videoId) {
+        return {
+          ...state,
+          [videoId]: videos(state[videoId], action),
+        };
+      }
+      return state;
+  }
+};
+
+const visibleIds = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_VIDEOS:
+      return action.videos.map((video) => video.id);
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  byId,
+  visibleIds,
+});
+
 export const getVideos = (state, action) => videos({ ...state }, action);
+export const getVideo = (state, id) => state.byId[id];
