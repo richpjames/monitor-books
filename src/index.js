28b36e75365edf61f022e510eef10718f41d4611
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-28 17:26:18 +0100

    add logger

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-
+import { createLogger } from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
+
 import reducer from "./reducers";
 import { getAllProducts, getAllVideos } from "./actions";
 import App from "./App";
 import { loadState, saveState } from "./localStorage";
 
 const middleware = [thunk];
-
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
 const persistedState = loadState();
 
 const store = createStore(
