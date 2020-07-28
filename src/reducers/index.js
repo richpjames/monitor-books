271e6c78eb8fd621f8184a9aa18234a936ea04df
richpjames <rich@Richards-MacBook-Pro.local>
2020-07-28 17:27:28 +0100

    use default import of videos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import cart, * as fromCart from "./cart";
 import products, * as fromProducts from "./products";
-import { videos } from "./videos";
+import videos from "./videos";
 
 export default combineReducers({
   cart,
