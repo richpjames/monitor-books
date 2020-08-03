0c969328d3ba21a496b26c62fe5be5426ec92fa2
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-03 20:56:16 +0100

    add Button logic

--- src/Components/Pages/BookPage.tsx
+++ src/Components/Pages/BookPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
+import { connect } from "react-redux";
 
+import { addToCart } from "../../actions";
 import Photos from "../Common/Photos";
 import Title from "../Common/Title";
 import Text from "../Common/Text";
@@ -9,11 +11,36 @@ import BuyButton from "../Common/BuyButton";
 
 interface IProps extends RouteComponentProps {
   book: Book;
+  cartQuantityById?: { [key: string]: number };
+  inventoryQuantity?: number;
+  addToCart?: (productId: string) => void;
 }
 
-const BookPage = (props: IProps) => {
-  const { book } = props;
-  const { photos, title, author, blurb1, blurb2 } = book;
+const BookPage = ({
+  book,
+  inventoryQuantity,
+  cartQuantityById,
+  addToCart,
+}: IProps) => {
+  const { photos, title, author, blurb1, blurb2, id } = book;
+
+  let buttonMessage;
+  let buttonDisabled;
+  if (
+    inventoryQuantity &&
+    inventoryQuantity > 0 &&
+    cartQuantityById &&
+    !cartQuantityById[id]
+  ) {
+    buttonMessage = "Add to basket";
+    buttonDisabled = false;
+  } else if (cartQuantityById && cartQuantityById[id] > 0) {
+    buttonMessage = "In basket";
+    buttonDisabled = true;
+  } else {
+    buttonMessage = "Out of stock";
+    buttonDisabled = true;
+  }
 
   return (
     <PageWrapper>
@@ -22,9 +49,18 @@ const BookPage = (props: IProps) => {
         <Title title={author} subtitle={title} />
         <Text leftText={blurb1} rightText={blurb2} />
       </InfoSection>
-      <BuyButton productId={book.id} />
+      {addToCart && (
+        <BuyButton onClick={() => addToCart(book.id)} disabled={buttonDisabled}>
+          {buttonMessage}
+        </BuyButton>
+      )}
     </PageWrapper>
   );
 };
 
-export default BookPage;
+const mapStateToProps = (state: State, ownProps: IProps) => ({
+  cartQuantityById: state.cart.quantityById,
+  inventoryQuantity: state.products.byId[ownProps.book.id].inventory,
+});
+
+export default connect(mapStateToProps, { addToCart })(BookPage);
