0870c86c609849d8acb6f64c3e04c1c68a0b89c0
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-16 08:38:41 +0100

    remove console.log

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -14,7 +14,6 @@ const OnRouteChangeWorker = ({ location, action }) => {
 const OnRouteChange = ({ action }) => (
   <Location>
     {({ location }) => {
-      console.log(location);
       return <OnRouteChangeWorker location={location} action={action} />;
     }}
   </Location>
