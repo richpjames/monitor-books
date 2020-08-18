75210f651cdad3a2b8f0982979b34d633a0796f6
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-18 21:35:19 +0100

    refactor

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-export const paragraphSplitter = (input: string) => {
-  return input.split("\n").map((i, key) => {
+export const paragraphSplitter = (input: string) =>
+  input.split("\n").map((i, key) => {
     return <p key={key}>{i}</p>;
   });
-};
