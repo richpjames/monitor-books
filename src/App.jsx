87ad95f60848d7451c2b46a2d0ce2b085f98b647
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-18 21:34:52 +0100

    fetch data on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 
 import "./App.css";
 import Slideshow from "./Components/Slideshow";
 import MainPage from "./Components/MainPage";
+import { fetchVideos, fetchProducts } from "./actions/index";
 
-const App = () => {
-  const [showImage, setShowImage] = useState(true);
-
+const App = ({ fetchVideos, fetchProducts }) => {
+  const [showImage, setShowImage] = useState(false);
   useEffect(() => {
+    fetchProducts();
+    fetchVideos();
     setTimeout(() => {
       setShowImage(false);
     }, 3000);
@@ -16,4 +19,4 @@ const App = () => {
   return <>{showImage ? <Slideshow /> : <MainPage />};</>;
 };
 
-export default App;
+export default connect(null, { fetchVideos, fetchProducts })(App);
