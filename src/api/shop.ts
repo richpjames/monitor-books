2e14ddc1a8354e7768f9e4c7721db2f860a11695
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-18 21:38:26 +0100

    use api functions

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -4,8 +4,8 @@ import { getBooks } from "./books";
 const TIMEOUT = 1;
 
 export default {
-  getProducts: async (cb: (callback: void) => void) => cb(await getBooks()),
-  getVideos: async (cb: (callback: void) => void) => cb(await getVideos()),
+  getProducts: getBooks,
+  getVideos: getVideos,
   buyProducts: (payload: any, cb: () => void, timeout: number) =>
     setTimeout(() => cb(), timeout || TIMEOUT),
 };
