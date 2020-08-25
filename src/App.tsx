906505e179c3dd5d2828599a920060d3ba415204
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-25 08:55:37 +0100

    convert to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,21 @@ import Slideshow from "./Components/Slideshow";
 import MainPage from "./Components/MainPage";
 import { fetchVideos, fetchProducts } from "./actions/index";
 
-const App = ({ fetchVideos, fetchProducts }) => {
+interface IProps {
+  fetchVideos: () => void;
+  fetchProducts: () => void;
+}
+
+const App = ({ fetchVideos, fetchProducts }: IProps) => {
   const [showImage, setShowImage] = useState(false);
+  console.log(process.env);
   useEffect(() => {
     fetchProducts();
     fetchVideos();
     setTimeout(() => {
       setShowImage(false);
     }, 3000);
-  }, []);
+  }, [fetchProducts, fetchVideos]);
 
   return <>{showImage ? <Slideshow /> : <MainPage />};</>;
 };
