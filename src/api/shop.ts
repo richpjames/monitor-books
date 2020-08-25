e2b45d46a171534da6a63997a73eb02dae2f3249
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-25 08:58:19 +0100

    add buyProducts async function

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -1,11 +1,9 @@
 import { getVideos } from "./videos";
 import { getBooks } from "./books";
-
-const TIMEOUT = 1;
+import { handleCheckout } from "./stripe-purchase";
 
 export default {
   getProducts: getBooks,
   getVideos: getVideos,
-  buyProducts: (payload: any, cb: () => void, timeout: number) =>
-    setTimeout(() => cb(), timeout || TIMEOUT),
+  buyProducts: handleCheckout,
 };
