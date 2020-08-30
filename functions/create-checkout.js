f2e7d09033f45ba96c2882fc0eaca78f3634e93f
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-30 21:13:22 +0100

    use imported list of countries

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -1,37 +1,32 @@
+import { SUPPORTED_LOCATIONS } from "./constants";
 const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
-const inventory = require("./data/books.json");
+const liveInventory = require("./data/live/books.json");
+const devInventory = require("./data/dev/books.json");
+
+const inventory =
+  process.env.NODE_ENV === "production" ? liveInventory : devInventory;
 
 exports.handler = async (event) => {
   const { id, quantity } = JSON.parse(event.body);
   const product = inventory.find((p) => p.id === id);
-  console.log(
-    `${product} product, inventory: ${inventory[0].id} id: ${id}, event: ${event}`
-  );
+
   const validatedQuantity = quantity > 0 < 11 ? quantity : 1;
-  console.log(product);
+  console.log(event, "event");
+
+  console.log(product, "product");
   const session = await stripe.checkout.sessions.create({
     mode: "payment",
     payment_method_types: ["card"],
-    billing_address_collection: "auto",
+    billing_address_collection: "required",
     shipping_address_collection: {
-      allowed_countries: ["US", "CA", "GB"],
+      allowed_countries: SUPPORTED_LOCATIONS,
     },
     success_url: `${process.env.URL}/success.html`,
     cancel_url: process.env.URL,
 
     line_items: [
       {
-        price_data: {
-          currency: "usd",
-          unit_amount: product.price,
-          product_data: {
-            name: product.title,
-            description: product.blurb1,
-            // // images: [product.image],
-            // amount: product.price,
-            // currency: product.currency,
-          },
-        },
+        price: "price_1HF3WIJs9ciiqN7OElxdqBnZ",
         quantity: validatedQuantity,
       },
     ],
