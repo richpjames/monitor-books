6a57611b82897577d6240d30265de796790990d8
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-30 21:13:41 +0100

    add env dependent data

--- functions/get-books.js
+++ functions/get-books.js
@@ -1,6 +1,9 @@
-const products = require("./data/books.json");
+const testProducts = require("./data/dev/books.json");
+const liveProducts = require("./data/live/books.json");
 
 exports.handler = async () => {
+  const products =
+    process.env.NODE_ENV === "production" ? liveProducts : testProducts;
   return {
     statusCode: 200,
     body: JSON.stringify(products),
