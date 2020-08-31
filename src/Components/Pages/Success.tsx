16c64442b2e6c641b5150fe4f810e02ff842ca75
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-31 10:50:26 +0100

    add success page

---
+++ src/Components/Pages/Success.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+interface Props extends RouteComponentProps {}
+export const Success = (props: Props) => {
+  return <div>success</div>;
+};
