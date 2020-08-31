27f868673cbfee265c594f225d3588ff1f058cc7
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-31 13:11:16 +0100

    add info section

--- src/Components/Pages/Success.tsx
+++ src/Components/Pages/Success.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components/macro";
 
 import { InfoSection } from "../Common/Common";
 import Title from "../Common/Title";
+import Text from "../Common/Text";
 
 const PageWrapper = styled.div`
   display: flex;
@@ -15,8 +16,13 @@ interface Props extends RouteComponentProps {}
 export const Success = (props: Props) => {
   return (
     <PageWrapper>
-      <Title title="Success" />
-      <InfoSection></InfoSection>
+      <InfoSection>
+        <Title title="Success" />
+        <Text
+          leftText={"payment confirmed.\n everything went through ok"}
+          rightText={"maybe display order here?"}
+        />
+      </InfoSection>
     </PageWrapper>
   );
 };
