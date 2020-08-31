f675de8b4e9215f87ae9a15cd729eb8e32342fdc
richpjames <rich@Richards-MacBook-Pro.local>
2020-08-31 21:02:00 +0100

    use Common PageWrapper

--- src/Components/Pages/Success.tsx
+++ src/Components/Pages/Success.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
-import styled from "styled-components/macro";
 
-import { InfoSection } from "../Common/Common";
+import { InfoSection, PageWrapper } from "../Common/Common";
 import Title from "../Common/Title";
 import Text from "../Common/Text";
-
-const PageWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-`;
 
 interface Props extends RouteComponentProps {}
 export const Success = (props: Props) => {
