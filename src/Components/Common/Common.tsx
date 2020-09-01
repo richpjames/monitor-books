53a9097f2ac96b0048ffe42504c8303a448294b8
richpjames <rich@Richards-MacBook-Pro.local>
2020-09-01 21:52:22 +0100

    remove page margin

--- src/Components/Common/Common.tsx
+++ src/Components/Common/Common.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components/macro";
 
 export const PageWrapper = styled.div`
   max-width: 100%;
-  margin-top: 3vh;
   display: flex;
   flex-direction: column;
   align-items: center;
