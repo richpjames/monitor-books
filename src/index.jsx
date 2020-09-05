a27b2b9d0a114b51214702de5c35a40d3ef7ef28
richpjames <rich@Richards-MacBook-Pro.local>
2020-09-05 18:31:32 +0100

    convert to jsx

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import reducer from "./reducers";
+import App from "./App";
+import { loadState, saveState } from "./sessionStorage";
+
+const middleware = [thunk];
+
+const initialState = loadState();
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
