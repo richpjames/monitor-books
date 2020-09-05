32e6d433ea79f579bb4867021559343b34c16f34
richpjames <rich@Richards-MacBook-Pro.local>
2020-09-05 20:36:39 +0100

    add VideoPage component

---
+++ src/Components/Pages/VideosPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+interface IProps extends RouteComponentProps {}
+
+export const VideosPage = (props: IProps) => {
+  return <div></div>;
+};
