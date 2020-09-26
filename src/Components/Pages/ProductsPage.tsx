cc9f4179be37bf6a787f947bf1e038864e1ca352
richpjames <rich@Richards-MacBook-Pro.local>
2020-09-26 20:46:29 +0100

    add styling and link properly

--- src/Components/Pages/ProductsPage.tsx
+++ src/Components/Pages/ProductsPage.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { RouteComponentProps, navigate } from "@reach/router";
+import React, { FunctionComponent } from "react";
+import { RouteComponentProps, navigate, Link } from "@reach/router";
 import styled from "styled-components/macro";
 
-import { mainImageUrl } from "../../constants";
+import { mainImageUrl, offWhite } from "../../constants";
 import {
   ListItemContainer,
   ListItemPhotoWrap,
@@ -11,6 +11,7 @@ import {
   ListItemTitle,
   ListItemSubtitle,
   PageWrapper,
+  AddToBasketButton,
 } from "../Common";
 import { PageTitle } from "../Common/Titles";
 
@@ -19,13 +20,18 @@ interface Props extends RouteComponentProps {
   bookIds: visibileIds;
 }
 
+const ButtonWrap = styled.div`
+  margin-top: auto;
+  padding-bottom: 2.5rem;
+`;
+
 const ListWrap = styled.section`
   display: flex;
   flex-wrap: wrap;
   padding-top: 2.5rem;
 `;
 
-export const ProductsPage = ({ books, bookIds }: Props) => {
+export const ProductsPage: FunctionComponent<Props> = ({ books, bookIds }) => {
   const bookDetails = bookIds
     .map((bookId) => books[bookId])
     .sort(
@@ -36,27 +42,37 @@ export const ProductsPage = ({ books, bookIds }: Props) => {
     <PageWrapper>
       <PageTitle>Books</PageTitle>
       <ListWrap>
-        {bookDetails.map((book, index) => (
-          <ListItemContainer
-            index={index}
-            height="50%"
-            width="100%"
-            horizontalMargin="0rem"
-            topMargin="1rem"
-            onClick={() => navigate(book.slug)}
-            key={index}
-          >
-            <ListItemPhotoWrap width="40%">
-              <ListItemPhoto
-                src={`${mainImageUrl}${book.imagePath}/thumbnails/${book.thumbnail}`}
-              />
-            </ListItemPhotoWrap>
-            <MetaInfoContainer index={index} width="40%">
-              <ListItemTitle>{book.title}</ListItemTitle>
-              <ListItemSubtitle>{book.author}</ListItemSubtitle>
-            </MetaInfoContainer>
-          </ListItemContainer>
-        ))}
+        {bookDetails.map((book, index) => {
+          const { slug, title, author, thumbnail, imagePath, id } = book;
+          return (
+            <ListItemContainer
+              index={index}
+              height="50%"
+              width="100%"
+              horizontalMargin="0rem"
+              topMargin="1rem"
+              key={index}
+              to={slug}
+            >
+              <ListItemPhotoWrap width="40%">
+                <ListItemPhoto
+                  src={`${mainImageUrl}${imagePath}/thumbnails/${thumbnail}`}
+                />
+              </ListItemPhotoWrap>
+              <MetaInfoContainer index={index} width="40%">
+                <ListItemTitle>{title}</ListItemTitle>
+                <ListItemSubtitle>{author}</ListItemSubtitle>
+                <ButtonWrap>
+                  <AddToBasketButton
+                    id={id}
+                    borderColour={offWhite}
+                    linkTo="/books"
+                  />
+                </ButtonWrap>
+              </MetaInfoContainer>
+            </ListItemContainer>
+          );
+        })}
       </ListWrap>
     </PageWrapper>
   );
