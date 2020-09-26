4ca245cea118ad82eb8fd509f4463cda523712a4
richpjames <rich@Richards-MacBook-Pro.local>
2020-09-26 20:43:24 +0100

    styling

--- src/Components/Common/Titles.tsx
+++ src/Components/Common/Titles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/macro";
+import { darkGrey } from "../../constants";
 
 export const PageSubtitle = styled.h1`
   font-family: "Caslon", sans-serif;
@@ -15,7 +16,7 @@ export const PageTitleBold = styled(PageTitle)`
 `;
 
 export const TitleWrapper = styled.div`
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid ${darkGrey};
   padding-top: 2rem;
   padding-bottom: 1.5rem;
   @media only screen and (min-width: 600px) {
