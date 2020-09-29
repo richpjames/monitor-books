049b789f2e05f8ce4d136bd081d480df46ca4f97
richpjames <rich@Richards-MacBook-Pro.local>
2020-09-29 21:32:29 +0100

    remove left margin

--- src/Components/Pages/Basket/ShippingCost.tsx
+++ src/Components/Pages/Basket/ShippingCost.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-const ShippingCostWrapper = styled.h5`
-  margin-left: auto;
-`;
+const ShippingCostWrapper = styled.h5``;
 
 interface Props {
   total: string;
