7a3dfeb3a5586ac8f563eaa6768386d1921abb2d
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-17 16:54:49 +0100

    add before each with state

--- cypress/integration/pages/basket.js
+++ cypress/integration/pages/basket.js
@@ -1,42 +1,44 @@
+import { addToBasket } from "../../../src/actions";
+
 const dispatch = (action) =>
   cy.window().its("store").invoke("dispatch", action);
-import { addToBasket } from "../../../src/actions";
-import { initialState } from "../../../src/constants/";
 
-// describe("Basket", () => {
-//   beforeEach(() =>
-//     cy.fixture("initialisedState").then((initialisedState) =>
-//       cy.visit("/basket", {
-//         onBeforeLoad: (win) => {
-//           win.initialState = initialisedState;
-//         },
-//       })
-//     )
-//   );
+describe("Basket", () => {
+  beforeEach(() =>
+    cy.fixture("initialisedState").then((initialisedState) =>
+      cy.visit("/basket", {
+        onBeforeLoad: (win) => {
+          win.initialState = initialisedState;
+        },
+      })
+    )
+  );
 
-//test correct items are shown in basket
-// xit("test correct items are shown in basket", () => {
-//   cy.fixture("initialisedState").then((initialisedState) => {
-//     dispatch(addToBasket("9T65B28LLM2MC"));
-//     cy.window()
-//       .its("store")
-//       .invoke("getState")
-//       .should("deep.equal", {
-//         ...initialisedState,
-//         cart: {
-//           addedIds: ["9T65B28LLM2MC"],
-//           quantityById: {
-//             "9T65B28LLM2MC": 1,
-//           },
-//           shipping: {
-//             region: "UK",
-//             priceId: "price_1HMwTgJs9ciiqN7OnYGR5rOp",
-//             price: 2.5,
-//           },
-//         },
-//       });}}
-//   });
-// });
+  // test correct items are shown in basket
+  xit("test correct items are shown in basket", () => {
+    cy.fixture("initialisedState").then((initialisedState) => {
+      dispatch(addToBasket("9T65B28LLM2MC"));
+      cy.window()
+        .its("store")
+        .invoke("getState")
+        .should("deep.equal", {
+          ...initialisedState,
+          cart: {
+            addedIds: ["9T65B28LLM2MC"],
+            quantityById: {
+              "9T65B28LLM2MC": 1,
+            },
+            shipping: {
+              region: "UK",
+              priceId: "price_1HMwTgJs9ciiqN7OnYGR5rOp",
+              price: 2.5,
+            },
+          },
+        });
+    });
+  });
+});
+
 //test increasing quantity of a book increases the price accordingly
 
 //test decreasing quantity of a book decreases the price accordingly
