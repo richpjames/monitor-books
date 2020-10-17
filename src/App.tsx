34841110283364ad125210727cec498c7aad7747
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-17 16:55:34 +0100

    use redux for slideshow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { navigate } from "@reach/router";
 
 import "./App.css";
 import Slideshow from "./Components/Global/Slideshow";
 import MainPage from "./Components/Pages/MainPage";
-import { fetchVideos, fetchProducts } from "./actions/index";
+import { fetchVideos, fetchProducts, setShowSlideshow } from "./actions/index";
 import { introTimer, homePage } from "./constants/";
 
 const introTimerMilliseconds = introTimer * 1000;
@@ -13,29 +13,48 @@ const introTimerMilliseconds = introTimer * 1000;
 interface Props {
   fetchVideos: () => void;
   fetchProducts: () => void;
+  setShowSlideshow: (showSlideshow: boolean) => void;
+  showSlideshow: boolean;
 }
 
-const App = ({ fetchVideos, fetchProducts }: Props) => {
-  const [showImage, setShowImage] = useState(false);
+const App = ({
+  fetchVideos,
+  fetchProducts,
+  setShowSlideshow,
+  showSlideshow,
+}: Props) => {
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      setTimeout(() => {
+        setShowSlideshow(!showSlideshow);
+        navigate(homePage);
+      }, introTimerMilliseconds);
+    }
+  }, [setShowSlideshow, showSlideshow]);
 
   useEffect(() => {
-    if (window.location.pathname === "/") setShowImage(true);
     fetchProducts();
     fetchVideos();
-    setTimeout(() => {
-      if (showImage) {
-        setShowImage(false);
-        navigate(homePage);
-      }
-    }, introTimerMilliseconds);
-  }, [fetchProducts, fetchVideos, showImage]);
+  }, [fetchProducts, fetchVideos, setShowSlideshow]);
 
   return (
     <>
-      {showImage && <Slideshow />}
-      {<MainPage hide={showImage} />}
+      {showSlideshow && <Slideshow />}
+      {<MainPage hide={showSlideshow} />}
     </>
   );
 };
 
-export default connect(null, { fetchVideos, fetchProducts })(App);
+const mapStateToProps = ({
+  config,
+}: {
+  config: { showSlideshow: boolean };
+}) => {
+  return { showSlideshow: config.showSlideshow };
+};
+
+export default connect(mapStateToProps, {
+  fetchVideos,
+  fetchProducts,
+  setShowSlideshow,
+})(App);
