81383d33b639b237d127f456da3412046a581b42
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-17 17:28:11 +0100

    move twoDecimalPlaces to utils

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-export const paragraphSplitter = (input: string = "") =>
+export const paragraphSplitter = (input = "") =>
   input.split("\n").map((i, key) => {
     return <p key={key}>{i}</p>;
   });
+
+export const twoDecimalPlaces = (number: number) =>
+  (Math.round(number * 100) / 100).toFixed(2);
