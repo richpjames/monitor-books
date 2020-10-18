ecaef5712fb7f68601cb90e62e3aa47c1784a755
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-18 21:41:22 +0100

    reduce bottom padding

--- src/Components/Common/Titles.tsx
+++ src/Components/Common/Titles.tsx
@@ -18,7 +18,7 @@ export const PageTitleBold = styled(PageTitle)`
 export const TitleWrapper = styled.div`
   border-bottom: 1px solid ${text};
   padding-top: 2rem;
-  padding-bottom: 1.5rem;
+  padding-bottom: 0.75rem;
   @media only screen and (min-width: 600px) {
     padding-bottom: 0.5rem;
     padding-top: 2rem;
