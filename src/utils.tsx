267d623c0419c6235dca50e42493680d4b01deca
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-18 21:28:38 +0100

    remove paragraph splitter

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,9 +1,2 @@
-import React from "react";
-
-export const paragraphSplitter = (input = "") =>
-  input.split("\n").map((i, key) => {
-    return <p key={key}>{i}</p>;
-  });
-
 export const twoDecimalPlaces = (number: number) =>
   (Math.round(number * 100) / 100).toFixed(2);
