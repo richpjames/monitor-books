0bed2be4884f3970082da23705b825dbe1d77739
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-20 21:11:08 +0100

    use page title

--- src/Components/Pages/Success.tsx
+++ src/Components/Pages/Success.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
 
-import { InfoSection, PageWrapper, Title, Text } from "../Common";
+import { InfoSection, PageWrapper, PageTitle, Text } from "../Common";
 
 export const Success: React.FC<RouteComponentProps> = () => {
   return (
     <PageWrapper>
       <InfoSection>
-        <Title title="Success" />
+        <PageTitle title="Success" />
         <Text
           text={`Everything went through ok. Thanks for your order. \n You will receive an email with details of your order. \n For enquiries contact@monitorbooks.co.uk`}
         />
