93500b3a816a4960ff0d6a07f3e62257b8c22fda
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-21 21:13:33 +0100

    always set showSlideshow to false

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = ({
   useEffect(() => {
     if (window.location.pathname === "/") {
       setTimeout(() => {
-        setShowSlideshow(!showSlideshow);
+        setShowSlideshow(false);
         navigate(homePage);
       }, introTimerMilliseconds);
     }
