82aa56906dd6720d7ff3b64caf152f713afddc5b
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-21 21:10:37 +0100

    add const colour for loading spinner

--- src/Components/Common/Common.tsx
+++ src/Components/Common/Common.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/macro";
+import { text } from "../../constants";
 
 export const PageWrapper = styled.div`
   padding: 2.5rem;
@@ -34,8 +35,8 @@ export const LoadingAnimation = styled.div`
     height: 64px;
     margin: 8px;
     border-radius: 50%;
-    border: 6px solid #000;
-    border-color: #000 transparent #000 transparent;
+    border: 6px solid ${text};
+    border-color: ${text} transparent ${text} transparent;
     animation: lds-dual-ring 1.2s linear infinite;
   }
   @keyframes lds-dual-ring {
