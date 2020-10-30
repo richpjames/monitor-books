1d3fac0b929af40880cff6c1aacacce4c5d0ae40
richpjames <rich@Richards-MacBook-Pro.local>
2020-10-30 20:36:35 +0000

    set slideshow false

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,8 @@ const App = ({
         navigate(`${productsPageName}/${homePage}`);
         setShowSlideshow(false);
       }, introTimerMilliseconds);
+    } else {
+      setShowSlideshow(false);
     }
     fetchProducts();
     fetchVideos();
