f74e26f54c5c5fbb31a0db747643a23094d0d439
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-01 10:13:24 +0000

    add CHECKOUT_SUCCESS dispatch

--- src/Components/Pages/Success.tsx
+++ src/Components/Pages/Success.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { RouteComponentProps } from "@reach/router";
+import { CHECKOUT_SUCCESS } from "../../constants/actionTypes";
 
 import { InfoSection, PageWrapper, PageTitle, Text } from "../Common";
 
 export const Success: React.FC<RouteComponentProps> = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: CHECKOUT_SUCCESS });
+  }, [dispatch]);
   return (
     <PageWrapper>
       <InfoSection>
         <PageTitle title="Success" />
         <Text
-          text={`<p>Everything went through ok. Thanks for your order. \n You will receive an email with details of your order. \n For enquiries contact@monitorbooks.co.uk</p>`}
+          text={`<p>Everything went through ok.<br /> Thanks for your order. <br /> You will receive an email with details. <br /> For enquiries contact@monitorbooks.co.uk</p>`}
         />
       </InfoSection>
     </PageWrapper>
