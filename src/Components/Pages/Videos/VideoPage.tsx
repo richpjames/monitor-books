0606d0145aff346391cefbb2c20e6bd564bdadde
richpjames <rich@Richards-MacBook-Pro.local>
2020-12-15 20:46:03 +0000

    make header correct size

--- src/Components/Pages/Videos/VideoPage.tsx
+++ src/Components/Pages/Videos/VideoPage.tsx
@@ -1,22 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import { RouteComponentProps } from "@reach/router";
 
-import { InfoSection, Video, VideoTitle, SplitText } from "../../Common";
+import {
+  InfoSection,
+  Video,
+  VideoTitle,
+  SplitText,
+  PageWrapper,
+} from "../../Common";
 
 interface Props extends RouteComponentProps {
   video: Video;
 }
 
+const VideoPageWrapper = styled(PageWrapper)`
+  display: block;
+`;
+
 export default function VideoPage(props: Props) {
   const { url, title, creators, description1, description2 } = props.video;
   React.useEffect(() => window.scrollTo(0, 0), []);
   return (
-    <>
-      <Video url={url} title={title} />
-      <InfoSection>
-        <VideoTitle title={`${title}:`} subtitle={creators} />
-        <SplitText leftText={description1} rightText={description2} />
-      </InfoSection>
-    </>
+    <VideoPageWrapper>
+      <>
+        <Video url={url} title={title} />
+        <InfoSection>
+          <VideoTitle title={`${title}:`} subtitle={creators} />
+          <SplitText leftText={description1} rightText={description2} />
+        </InfoSection>
+      </>
+    </VideoPageWrapper>
   );
 }
