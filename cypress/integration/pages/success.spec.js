d8407b9db5c0a80f18c06fa145805deaa4f762a8
richpjames <rich@Richards-MacBook-Pro.local>
2020-12-30 22:00:22 +0000

    fix: success page test

--- cypress/integration/pages/success.spec.js
+++ cypress/integration/pages/success.spec.js
@@ -2,11 +2,7 @@ const dispatch = (action) =>
   cy.window().its("store").invoke("dispatch", action);
 
 describe("Basket", () => {
-  beforeEach(() =>
-    cy.visit(
-      "/sucess?session_id=cs_test_dLmRCxB2KLmWk5J03ZU17OOzncuSMWmeHQ6nCauM4pzulAQa86Vy92kn"
-    )
-  );
+  beforeEach(() => cy.visit("/success"));
   it("has the heading 'Success'", () => {
     cy.get("h1").contains("Success");
   });
