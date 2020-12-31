bf268b06e96541f392ca0c9be75b46245c8575bd
richpjames <rich@Richards-MacBook-Pro.local>
2020-12-31 12:50:45 +0000

    fix: add env awareness to api

--- src/api/shop.ts
+++ src/api/shop.ts
@@ -1,10 +1,11 @@
 import { getVideos } from "./videos";
-// import { getDevBooks } from "./devBooks";
+import { getDevBooks } from "./devBooks";
 import { getProdBooks } from "./prodBooks";
 import { handleCheckout } from "./stripe-purchase";
 
 export default {
-  getProducts: getProdBooks,
+  getProducts:
+    process.env.NODE_ENV === "development" ? getDevBooks : getProdBooks,
   getVideos: getVideos,
   buyProducts: handleCheckout,
 };
