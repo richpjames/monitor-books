8d117fffd4084ec1ed7b9bb51e2071aa398731c1
richpjames <rich@Richards-MacBook-Pro.local>
2021-06-24 08:49:00 +0100

    add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
