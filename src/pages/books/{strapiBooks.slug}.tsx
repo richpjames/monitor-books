1d21a2dff1b59b83fbdc0c70e455852212f60bbf
richpjames <rich@Richards-MacBook-Pro.local>
2021-06-29 08:41:47 +0100

    style(strapiBooks.slug): Remove unused import

--- src/pages/books/{strapiBooks.slug}.tsx
+++ src/pages/books/{strapiBooks.slug}.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components/macro";
 import ReactMarkdown from "react-markdown";
 
 import { productMapper } from "../../api/mappers";
-import { sanitizeText } from "../../utils";
 
 import Layout from "../../Components/layout";
 import { AddToBasketButton, Photos } from "../../Components/Common";
