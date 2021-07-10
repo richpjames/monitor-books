67d9be843a66844eb3419ef4667bdac831296976
richpjames <rich@Richards-MacBook-Pro.local>
2021-07-10 13:29:55 +0100

    remove intro timer

--- src/Components/layout.tsx
+++ src/Components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components/macro";
 
 import "../css/app.css";
@@ -46,13 +46,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({
   children,
-  pathname,
 }) => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      clearTimeout((window as any).introTimer);
-    }
-  }, [pathname]);
 
   return (
     <PageWrapper>
