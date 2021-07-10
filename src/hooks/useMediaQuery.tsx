01ef60cbe8ef6ff344f15ab6e5a951b152bd3039
richpjames <rich@Richards-MacBook-Pro.local>
2021-07-10 13:30:34 +0100

    create useIsMobile hook

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -2,36 +2,34 @@ import React from "react";
 
 type HandleMQLChange = (this: MediaQueryList, env: MediaQueryListEvent) => void;
 
-const useMediaQuery = (query: string) => {
-  const [match, setMatch] = React.useState(false);
+export const useMediaQuery = (query: string) => {
+    const [match, setMatch] = React.useState(false);
 
-  const mqlRef = React.useRef<MediaQueryList | null>(null);
+    const mqlRef = React.useRef<MediaQueryList | null>(null);
 
-  React.useEffect(() => {
-    let shouldUpdate = true;
+    React.useEffect(() => {
+        let shouldUpdate = true;
 
-    if (window && window.matchMedia) {
-      mqlRef.current = window.matchMedia(query);
-    }
-    const { current: mql } = mqlRef;
+        if (window && window.matchMedia) {
+            mqlRef.current = window.matchMedia(query);
+        }
+        const { current: mql } = mqlRef;
 
-    if (mql && match !== mql.matches) {
-      setMatch(mql.matches);
-    }
+        if (mql && match !== mql.matches) {
+            setMatch(mql.matches);
+        }
 
-    const handleChange: HandleMQLChange = ({ matches }) => {
-      if (shouldUpdate) setMatch(matches);
-    };
+        const handleChange: HandleMQLChange = ({ matches }) => {
+            if (shouldUpdate) setMatch(matches);
+        };
 
-    mql?.addEventListener("change", handleChange);
+        mql?.addEventListener("change", handleChange);
 
-    return () => {
-      shouldUpdate = false;
-      mql?.removeEventListener("change", handleChange);
-    };
-  }, [query, match, setMatch]);
+        return () => {
+            shouldUpdate = false;
+            mql?.removeEventListener("change", handleChange);
+        };
+    }, [query, match, setMatch]);
 
-  return match;
+    return match;
 };
-
-export default useMediaQuery;
