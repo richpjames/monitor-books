8ee090f7c356a5152b415c600237d751df8b3b2e
richpjames <rich@Richards-MacBook-Pro.local>
2023-01-10 21:16:29 +0000

    fix yet more pages

---
+++ src/pages/success.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PageProps } from "gatsby";
+import Layout from "../Components/layout";
+import { useSetBackground } from "../hooks/useSetBackground";
+
+const Success: React.FC<PageProps> = () => {
+  useSetBackground("basket");
+  return (
+    <Layout>
+      <h1>Order Successful</h1>
+      <p>
+        Everything went through ok.
+        <br /> Thanks for your order. <br /> You will receive an email with
+        details. <br /> For enquiries editor@monitorbooks.co.uk
+      </p>
+    </Layout>
+  );
+};
+export default Success;
